706047c907bdc708961ee3b67b7439eed7500138
Maxfield Gaj <maxfieldegaj@gmail.com>
2024-10-15 21:31:27 -0400

    compose functions

--- good-habits/main.py
+++ good-habits/main.py
@@ -1,12 +1,24 @@
-def greet() -> None:
-    print('Hello, world!')
+def is_an_adult(age: int, has_id: bool) -> bool:
+    return age >= 21 and has_id
 
-def bye() -> None:
-    print('Bye, world!')
+def is_bob(name: str) -> bool:
+    return name.lower() == 'bob'
+
+def enter_club(name: str, age: int, has_id: bool) -> None:
+    if is_bob(name):
+        print('Get out of here Bob, we don\'t want no trouble.')
+        return
+
+    if is_an_adult(age, has_id):
+        print('You may enter club')
+    else:
+        print('You may not enter the clurrb')
 
 def main() -> None:
-    greet()
-    bye()
+    enter_club('Bob', 29, has_id=True)
+    enter_club('James', 29, has_id=True)
+    enter_club('Sandra', 29, has_id=False)
+    enter_club('Mario', 20, has_id=True)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
