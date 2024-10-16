dd621b62e568a162e58e0328d99d9436c405ee31
Maxfield Gaj <maxfieldegaj@gmail.com>
2024-10-15 21:16:01 -0400

    Finish shafer course

--- intro.py
+++ intro.py
@@ -1,2 +1,17 @@
+from my_module import *
+import datetime
+import os
+import antigravity
 
 courses = ['History', 'Math', 'Physics', 'CompSci']
+
+index = find_index(courses, 'Math')
+# print(index)
+# print(test)
+# print(sys.path)
+
+today = datetime.date.today()
+print(today)
+
+print(os.__file__)
+print(antigravity.geohash)
