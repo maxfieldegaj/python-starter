f70475179afaeaf5c214a23dc1fb733281ca4e2c
Maxfield Gaj <maxfieldegaj@gmail.com>
2024-10-15 21:24:04 -0400

    main function

--- good-habits/main.py
+++ good-habits/main.py
@@ -1,4 +1,12 @@
-from module import connect
+def greet() -> None:
+    print('Hello, world!')
+
+def bye() -> None:
+    print('Bye, world!')
+
+def main() -> None:
+    greet()
+    bye()
 
 if __name__ == '__main__':
-    connect()
+    main()
